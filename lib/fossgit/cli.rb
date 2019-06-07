be9fd1bc540226be6aad86cb58a611799ebdf931
ren <ren@noemail.net>
2019-06-07 05:33:42 +0000

    add config info to help output
    FossilOrigin-Name: eccc6adc14101c1973c3e46e6e8b5f71210fc786

--- lib/fossgit/cli.rb
+++ lib/fossgit/cli.rb
@@ -2,21 +2,20 @@ class CLI
   def self.help_text name='fossgit'
     help = <<-EOF
 
-      FossGit provides a simple tool for creating Git mirrors of Fossil
-      repositories.  To use it, you need:
+      FossGit mirrors Fossil repositories to Git repositories.  You need:
       
       1. both Fossil SCM and Git installed
-      2. a Fossil repository to mirror with an open checkout
+      2. a Fossil repository with an open checkout to export
       3. a Git repository to use as a mirror
 
       USAGE:  #{name} -h
-              #{name} [-c <CHECKOUT>] [-l] <GITREPO>
+              #{name} [-c <CHECKOUT>] [-l] [GITREPO]
               #{name} -t
 
           By default, when exporting to local Git repository GITREPO, #{name}
           attempts to push updates to a configured upstream Git repository.  It
-          harmlessly fails to push if no upstream repository is configured for
-          the local Git repository.
+          should harmlessly fail to push if there is no upstream repository
+          configured for the local Git repository.
 
       OPTIONS/ARGUMENTS:
 
@@ -33,11 +32,13 @@ class CLI
                           default, #{name} tries to push to an upstream Git
                           repository whenever it exports from Fossil to Git.
 
-          -t              Dump export to STDOUT rather than sending it to Git.
-                          This overrides the `-l` option and GITREPO argument,
-                          if present.
+          -t              Dump export to STDOUT as text rather than sending it
+                          to Git.  This overrides the `-l` option and GITREPO
+                          argument, if present.
 
           GITREPO         Specify the location of your local Git repository.
+                          This is optional, if there is a configuration file
+                          that specifies it for you.
 
       EXAMPLES:
 
@@ -46,6 +47,35 @@ class CLI
           $ fossgit -c ~/fossil_checkouts/projectname ~/git/projectname
 
           $ cd ~/fossil_checkouts/projectname && fossgit ~/git/projectname
+
+      CONFIG:
+
+          A config file named ".fossgit" can be used to set #{name} defaults.
+
+          A universal config file can be placed in the home directory of the
+          user account that executes this command, in which case it will apply
+          every time that account executes #{name}.  A config file can also be
+          placed in the root of an open Fossil repository, in which case it
+          will only apply when #{name} is used for that particular repository.
+          If both files exist, settings from both will be used, but where both
+          have the same setting the more specific config (in the project
+          directory) overrides the equivalent setting from the universal
+          config.  This means a universal configuration can be used for the
+          most common case, but for specific projects that have different needs
+          you can place a config file the project repository.
+
+          There are two configuration options:
+
+          * The "gitdir" setting can be used to specify a directory in which
+            Git repositories are kept.  In this case, #{name} infers the Git
+            repository name from the "project-name" in "fossil info" output.
+
+          * The "gitrepo" setting can be used to specify the full path to a Git
+            repository, without letting #{name} try to infer the repository
+            name.  This option overrides "gitdir" if both are set.  It is
+            probably a bad idea to set "gitrepo" in a universal config.  It is
+            intended to be set in a project-specific ".fossgit" file because
+            chaos may ensue if all Fossil repositories use the same "gitrepo".
 
     EOF
   end
